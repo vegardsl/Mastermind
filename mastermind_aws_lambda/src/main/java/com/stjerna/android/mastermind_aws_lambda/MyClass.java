015293ed1d7b631964af84367f1011653b2c2794
vegardsl <vegstjli@hotmail.com>
2020-02-14 15:36:35 +0100

    Reorganized package structure.

--- mastermind_aws_lambda/src/main/java/com/stjerna/android/mastermind_aws_lambda/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.stjerna.android.mastermind_aws_lambda;
-
-import com.stjerna.android.mastermind_core.src.DummyClass;
-
-public class MyClass {
-
-}
---
+++ mastermind_aws_lambda/src/main/java/com/stjerna/mastermind/aws_lambda/Hello.java
@@ -0,0 +1,10 @@
+package com.stjerna.mastermind.aws_lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<String, String> {
+    public String handleRequest(String name, Context context) {
+        return String.format("Hello %s.", name);
+    }
+}
