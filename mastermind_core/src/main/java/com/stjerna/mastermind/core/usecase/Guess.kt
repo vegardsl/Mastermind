7edc20800a29feee42b3881a636b7bbe3bd8cc20
vegardsl <vegstjli@hotmail.com>
2020-02-15 13:23:22 +0100

    Guesses are updated.

--- mastermind_core/src/main/java/com/stjerna/mastermind/core/usecase/Guess.kt
+++ mastermind_core/src/main/java/com/stjerna/mastermind/core/usecase/Guess.kt
@@ -1,5 +1,26 @@
 package com.stjerna.mastermind.core.usecase
 
-class Guess {
+import com.stjerna.mastermind.core.Failure
+import com.stjerna.mastermind.core.Success
+import com.stjerna.mastermind.core.Try
+import com.stjerna.mastermind.core.entity.GameGateway
+import com.stjerna.mastermind.core.entity.MastermindGame
+import com.stjerna.mastermind.core.usecase.score.Score
+
+class Guess(private val storage: GameGateway) {
+    fun execute(gameID: String, guess: String): Try<MastermindGame> {
+        return when (val result = storage.get(gameID)) {
+            is Success -> scoreGuessAndUpdate(guess, result.value)
+            is Failure -> Failure(result.e)
+        }
+    }
+
+    private fun scoreGuessAndUpdate(
+        guess: String,
+        game: MastermindGame
+    ): Try<MastermindGame> {
+        game.guesses.add(Pair(guess, Score(0, 0)))
+        return Success(game)
+    }
 
 }
--- mastermind_core/src/test/java/com/stjerna/mastermind/core/usecase/GuessTest.kt
+++ mastermind_core/src/test/java/com/stjerna/mastermind/core/usecase/GuessTest.kt
@@ -1,11 +1,63 @@
 package com.stjerna.mastermind.core.usecase
 
+import com.stjerna.mastermind.core.Failure
+import com.stjerna.mastermind.core.Success
+import com.stjerna.mastermind.core.Try
+import com.stjerna.mastermind.core.entity.MastermindGame
+import com.stjerna.mastermind.core.entity.StubbedGameGateway
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.fail
+import java.io.IOException
 
 internal class GuessTest {
     @Test
-    fun nothing() {
+    fun createGuess() {
+        Guess(FakeGameGateway())
+    }
 
+    @Test
+    fun invokeGuess() {
+        Guess(FakeGameGateway()).execute(gameID = "GameID", guess = "CODE")
+    }
+
+    @Test
+    fun gameNotFound() {
+        when (val result = Guess(FakeGameGateway()).execute("GameID", guess = "CODE")) {
+            is Failure -> assertTrue(result.e is IOException)
+            is Success -> fail("Expected a failure.")
+        }
+    }
+
+    @Test
+    fun guessIsUpdated() {
+        val testGameID = "GameID"
+
+        val storage = FakeGameGateway(MastermindGame(testGameID, "ABCD", false))
+
+        when (val result = Guess(storage).execute(gameID = testGameID, guess = "CODE")) {
+            is Failure -> fail(result.e) // assertTrue(result.e is IOException)
+            is Success -> {
+                assertEquals(1, result.value.guesses.size)
+            }
+        }
+    }
+}
+
+class FakeGameGateway(
+    private val gameToReturn: MastermindGame? = null
+) : StubbedGameGateway() {
+
+    override fun put(game: MastermindGame): Try<Unit> {
+        return Success(Unit)
+    }
+
+    override fun get(gameID: String): Try<MastermindGame> {
+        return if (gameToReturn == null) {
+            Failure(IOException("Game with ID, \"$gameID\" not found."))
+        } else {
+            Success(gameToReturn)
+        }
     }
 }
