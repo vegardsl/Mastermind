cdd006f9fedff2a9a64a2e924eebe013e2089fca
vegardsl <vegstjli@hotmail.com>
2020-02-16 17:23:00 +0100

    Refactored the code scorer.

--- mastermind_core/src/main/java/com/stjerna/mastermind/core/usecase/score/CodeScorer.kt
+++ mastermind_core/src/main/java/com/stjerna/mastermind/core/usecase/score/CodeScorer.kt
@@ -5,8 +5,8 @@ class CodeScorer(code: String, guess: String) {
     val isPerfectScore: Boolean
     val score: Score
 
-    val positionsUsed = booleanArrayOf(false, false, false, false)
-    val guessUsed = booleanArrayOf(false, false, false, false)
+    private val positionsUsed = booleanArrayOf(false, false, false, false)
+    private val guessUsed = booleanArrayOf(false, false, false, false)
 
     init {
         require(code.length == GameRuleSet.codeSize)
@@ -22,9 +22,18 @@ class CodeScorer(code: String, guess: String) {
             require(GameRuleSet.symbolList.contains(guessSymbol))
         }
 
-        var correctSymbols = 0
-        var correctPositions = 0
+        val correctSymbols = countCorrectSymbols(guessList, codeList)
+        val correctPositions = countCorrectPositions(guessList, codeList)
 
+        score = Score(correctPositions, correctSymbols)
+        isPerfectScore = correctPositions == GameRuleSet.codeSize
+    }
+
+    private fun countCorrectPositions(
+        guessList: List<String>,
+        codeList: List<String>
+    ): Int {
+        var correctPositions = 0
         for (codePos in guessList.indices) {
             val guessSymbol = guessList[codePos]
             if (codeList[codePos] == guessSymbol) {
@@ -32,17 +41,37 @@ class CodeScorer(code: String, guess: String) {
                 guessUsed[codePos] = true
                 correctPositions++
             }
+        }
+        return correctPositions
+    }
+
+    private fun countCorrectSymbols(
+        guessList: List<String>,
+        codeList: List<String>
+    ): Int {
+        var correctSymbols = 0
+        for (codePos in guessList.indices) {
             for (guessPos in codeList.indices) {
-                if (codePos != guessPos && codeList[codePos] == guessList[guessPos] && !positionsUsed[codePos] && !guessUsed[guessPos]) {
+                if (isCorrectSymbolInWrongPosition(codePos, guessPos, codeList, guessList)) {
                     positionsUsed[codePos] = true
                     guessUsed[guessPos] = true
                     correctSymbols++
                 }
             }
         }
+        return correctSymbols
+    }
 
-        score = Score(correctPositions, correctSymbols)
-        isPerfectScore = correctPositions == GameRuleSet.codeSize
+    private fun isCorrectSymbolInWrongPosition(
+        codePos: Int,
+        guessPos: Int,
+        codeList: List<String>,
+        guessList: List<String>
+    ): Boolean {
+        return codePos != guessPos &&
+                codeList[codePos] == guessList[guessPos] &&
+                !positionsUsed[codePos] &&
+                !guessUsed[guessPos]
     }
 
 
