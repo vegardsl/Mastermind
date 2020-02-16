2f1e5c755f6dd281a57f007fc0f522d5473e37a4
vegardsl <vegstjli@hotmail.com>
2020-02-16 13:11:44 +0100

    Completed the guess and score view.

--- app/src/main/java/com/stjerna/android/mastermind/MainActivity.kt
+++ app/src/main/java/com/stjerna/android/mastermind/MainActivity.kt
@@ -3,10 +3,14 @@ package com.stjerna.android.mastermind
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.stjerna.mastermind.core.usecase.score.CodeScorer
+import com.stjerna.mastermind.core.usecase.score.Score
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scoreView: ScoreView = findViewById(R.id.scoreView)
+        scoreView.setGuess("ABCD")
+        scoreView.setScore(Score(1,2))
     }
 }
---
+++ app/src/main/java/com/stjerna/android/mastermind/ScoreView.kt
@@ -0,0 +1,51 @@
+package com.stjerna.android.mastermind
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.stjerna.mastermind.core.usecase.score.Score
+
+class ScoreView(context: Context, attributeSet: AttributeSet) :
+    LinearLayout(context, attributeSet) {
+
+    private val scoreItemList: List<View>
+
+    private val guessText: TextView
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.guess_view, this)
+
+        scoreItemList = listOf(
+            view.findViewById(R.id.score_1_1),
+            view.findViewById(R.id.score_1_2),
+            view.findViewById(R.id.score_2_1),
+            view.findViewById(R.id.score_2_2)
+        )
+
+        guessText = view.findViewById(R.id.guess_textView)
+    }
+
+
+
+    fun setGuess(guess: String) {
+        guessText.text = guess
+    }
+
+    fun setScore(score: Score) {
+        for (i in 0 until score.correctPositions) {
+            scoreItemList[i].setBackgroundColor(
+                ContextCompat.getColor(context, R.color.correctPosition)
+            )
+        }
+        for (i in score.correctPositions until score.correctSymbol + score.correctPositions) {
+            scoreItemList[i].setBackgroundColor(
+                ContextCompat.getColor(context, R.color.correctSymbol)
+            )
+        }
+    }
+}
